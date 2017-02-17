cfc52ee63aa7e319494b5d8015709e525b648916
César Sandrigo <cmsandrigo@gmail.com>
2017-02-17 17:34:38 -0300

    Initial jhipster app

---
+++ src/main/java/com/folderit/projector/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.folderit.projector.repository;
+
+import com.folderit.projector.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
